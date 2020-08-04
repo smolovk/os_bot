64442a49413b02404089da0310eeac5286d6e89d
smolovk <smolov326@gmail.com>
2020-08-04 15:57:22 +0300

    bvbvefevv

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import cv2
 
-bot = telebot.TeleBot("1355354595:AAGouNLuE0MvvJ_3tfLMAL2IGxHodRnOAYQ", parse_mode=None)
+bot = telebot.TeleBot("1303574038:AAFXtPPeRwatYsxpp6qsLgqmQPp7jyAZFBs", parse_mode="HTML")
 
  
 from PIL import ImageFont
@@ -12,13 +12,13 @@ from PIL import ImageDraw
 
 def create_img(text, filename):
     # определяете шрифт
-    font = ImageFont.truetype('Montserrat/Montserrat-Regular.ttf', 14)
+    font = ImageFont.truetype('Montserrat/Montserrat-Regular.ttf', 52)
 
     # определяете положение текста на картинке
-    text_position = (5, 70)
+    text_position = (15, 380)
 
     # цвет текста, RGB
-    text_color = (0,0,0)
+    text_color = (0, 0, 0)
 
     # загружаете фоновое изображение
     img = Image.open('blank.jpg')
@@ -36,7 +36,18 @@ def create_img(text, filename):
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Hello")
+	bot.reply_to(message, """Отправь мне любой стих в таком виде:\n
+У лукоморья дуб зелёный;
+Златая цепь на дубе том:
+И днём и ночью кот учёный
+Всё ходит по цепи кругом;
+Идёт направо — песнь заводит,
+Налево — сказку говорит.
+Там чудеса: там леший бродит,
+Русалка на ветвях сидит;
+
+<b>! ВНИМАНИЕ !</b> - <i><b>БЕЗ</b></i> пустых строк
+""")
 
 @bot.message_handler(func=lambda message: True)
 def get_text(message):
@@ -44,6 +55,19 @@ def get_text(message):
 
     textArr = text.split("\n")
     output = []
+
+    isFour = False
+
+    if len(textArr) % 4 == 0:
+        isFour = True
+    else:
+        isFour = False
+
+    if isFour == False:
+        while len(textArr) % 4 != 0:
+            textArr.append("")
+    else:
+        pass
 
     for i in range(len(textArr)):
         ii = 1 + i
